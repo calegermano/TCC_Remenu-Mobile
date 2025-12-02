9b7278b676770d2b7efa6a77a13c6403ced4b1e9
calegermano <carlosalexandre071221@gmail.com>
2025-12-02 00:08:17 -0300

    inicio do app

---
+++ scr/services/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+//colocar seu ip nesta baseURL para o funcionamento da api e ligar o site laravel pelo php artisan serve
+//ip Carlos: 187.10.13.65
+//lembrem que o ip pode mudar ent caso nao funcione digite no cmd o seguinte comando curl https://api.ipify.org
+const api = axios.create({
+    baseURL: 'http://xxx.xxx.xxx:8000/api', 
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
+});
+
+export default api;
